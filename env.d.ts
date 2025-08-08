054300c6a39ed3ba0466cf36c7f9d285d40193e1
hijal <hijikeshhijal@gmail.com>
2025-08-08 16:46:33 +0600

    fix: env import cloudflare

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,15 @@
 /// <reference types="@sveltejs/kit" />
 
-declare module '$env/dynamic/public' {
-    // example:
+declare module '$env/static/public' {
     export const PUBLIC_API_HOST: string;
     export const PUBLIC_NODE_ENV: 'development' | 'production';
+}
+
+declare module '$env/dynamic/public' {
+    export const env: {
+        PUBLIC_API_HOST: string;
+        PUBLIC_NODE_ENV: 'development' | 'production';
+    };
 }
 
 declare module '$env/dynamic/private' {
